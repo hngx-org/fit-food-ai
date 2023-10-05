2f23222a4c0b9d7cc222872aaee1108ed799e393
N-DiLo <egbekwunwanedilobu@gmail.com>
2023-10-05 16:45:14 +0100

    feat: added payment package and edit profile functionalities

--- lib/features/authentication/views/upgrade_plan.dart
+++ lib/features/authentication/views/upgrade_plan.dart
@@ -15,11 +15,11 @@ class UpgradePlan extends StatefulWidget {
 }
 
 class _UpgradePlanState extends State<UpgradePlan> {
-  String _selectedPlan = '';
+  String selectedPlan = '';
 
-  void checkedPlan(String value) {
+  void picked(String value) {
     setState(() {
-      _selectedPlan = value;
+      selectedPlan = value;
     });
   }
 
@@ -32,11 +32,6 @@ class _UpgradePlanState extends State<UpgradePlan> {
 
   @override
   Widget build(BuildContext context) {
-    var amount = _selectedPlan.substring(
-      0,
-      _selectedPlan.indexOf(' '),
-    );
-
     final planUpgrade = HNGPay();
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
@@ -45,7 +40,7 @@ class _UpgradePlanState extends State<UpgradePlan> {
       appBar: AppBar(
         leading: IconButton(
           onPressed: () {
-            Navigator.pushReplacementNamed(context, '/settings');
+            Navigator.pop(context);
           },
           icon: const Icon(
             IconlyLight.arrow_left_2,
@@ -118,67 +113,67 @@ class _UpgradePlanState extends State<UpgradePlan> {
                 height: height * 0.01,
               ),
               Container(
-                height: height * 0.2,
+                height: height * 0.23,
                 width: width,
                 decoration: BoxDecoration(
                   color: kcChatRcvdColor,
                   borderRadius: BorderRadius.circular(10.0),
                 ),
-                padding: const EdgeInsets.all(15),
-                child: ListView.builder(
-                  itemCount: 2, // Two options: Monthly and Yearly
-                  itemBuilder: (context, index) {
-                    final planName =
-                        index == 0 ? 'Monthly Plan' : 'Yearly Plan';
-                    final planCost =
-                        index == 0 ? '\$3 monthly' : '\$30 anually';
-
-                    return Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              Radio(
-                                value: index,
-                                groupValue: _selectedPlan,
-                                onChanged: (value) {
-                                  checkedPlan(value as String);
-                                },
-                              ),
-                              Text(
-                                planName,
-                                style: infoText,
-                              ),
-                            ],
-                          ),
-                          Text(
-                            planCost,
-                            style: smallText.copyWith(
-                              fontSize: 13,
-                            ),
-                          ),
-                        ],
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text(
+                        'Monthly Plan',
+                        style: infoText,
                       ),
-                    );
-                  },
+                      subtitle: Text(
+                        '\$3 monthly',
+                        style: smallText.copyWith(
+                          fontSize: 13,
+                        ),
+                      ),
+                      leading: Radio(
+                        value: '3',
+                        groupValue: selectedPlan,
+                        onChanged: (value) {
+                          picked(value as String);
+                        },
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(
+                        'Yearly Plan',
+                        style: infoText,
+                      ),
+                      subtitle: Text(
+                        '\$30 annually',
+                        style: smallText.copyWith(
+                          fontSize: 13,
+                        ),
+                      ),
+                      leading: Radio(
+                        value: '30',
+                        groupValue: selectedPlan,
+                        onChanged: (value) {
+                          picked(value as String);
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
               SizedBox(
                 height: height * 0.035,
               ),
-              Center(
-                child: Container(
-                  width: double.infinity,
-                  height: height * 0.065,
-                  decoration: BoxDecoration(
-                    color: kcBtnColor,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: planUpgrade.applePay(
-                    amountToPay: amount,
-                  ),
+              Container(
+                width: width,
+                height: height * 0.065,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: planUpgrade.applePay(
+                  amountToPay: selectedPlan,
                 ),
               ),
             ],
